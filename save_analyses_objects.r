c1f6326b70bd15950e8aae45d7218170bab087ab
edo98811 <filippie@uni-mainz.de>
2025-01-16 13:56:22 +0100

    added saving of gtl

--- save_analyses_objects.r
+++ save_analyses_objects.r
@@ -3,7 +3,8 @@ save_analysis_objects <- function(
     myresuSet, 
     dds, 
     dds_releveled = NULL, 
-    dds_interaction_term = NULL) {
+    dds_interaction_term = NULL, 
+    gtl = NULL) {
 
     if (is.character(params$experiment_name) & length(params$experiment_name)) {
         dir.create(file.path("analyses_data", params$experiment_name), showWarnings = FALSE, recursive = TRUE)
@@ -19,6 +20,10 @@ save_analysis_objects <- function(
             saveRDS(dds_interaction_term, file.path("analyses_data", params$experiment_name, "dds_DE_results_interaction_term.RDS"))
             message("dds_interaction_term saved successfully.")
         }
+        if (!is.null(gtl)) {
+            saveRDS(gtl, file.path("analyses_data", params$experiment_name, "gtl.RDS"))
+            message("gtl saved successfully.")
+        }
     } else {
         saveRDS(myresuSet, "myResuSet.RDS")
         message("myResuSet saved successfully.")
@@ -31,6 +36,10 @@ save_analysis_objects <- function(
         if (!is.null(dds_interaction_term)) {
             saveRDS(dds_interaction_term, "dds_DE_results_interaction_term.RDS")
             message("dds_interaction_term saved successfully.")
+        }
+        if (!is.null(gtl)) {
+            saveRDS(gtl, "gtl.RDS")
+            message("gtl saved successfully.")
         }
     }
 }
