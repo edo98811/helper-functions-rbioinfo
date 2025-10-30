c12f15bdc3126a7a2e0217ee8b38d002f3da363c
“edo98811” <filippie@uni-mainz.de>
2025-10-30 09:27:01 +0100

    added validate metadata function

--- R/validate_metadata.R
+++ R/validate_metadata.R
@@ -9,15 +9,28 @@
 #' @param se A `SummarizedExperiment` object. The function validates that the
 #' row names of the `metadata_df` match the row names of the `rowData` in this object.
 #'
-#' @return Returns `TRUE` if the row names match. If the row names do not match,
-#' the function throws an error.
+#' @return A `S4Vectors::DataFrame` containing the validated metadata.
+#' 
+#' @importFrom S4Vectors DataFrame
 #'
 #' @export
 validate_metadata <- function(metadata_df, se) {
-  # Check if the rownames of the metadata data.frame match the rownames of the rowData in the SummarizedExperiment object
-  if (!all(rownames(metadata_df) == rownames(rowData(se)))) {
-    stop("The rownames of the metadata data.frame do not match the rownames of the rowData in the SummarizedExperiment object.")
+  
+  # Check class
+  if (!inherits(metadata_df, "data.frame") && !inherits(metadata_df, "S4Vectors::DataFrame")) {
+    stop("`metadata_df` must be a data.frame or a S4Vectors::DataFrame.")
   }
-  return(TRUE)
+  
+  # Check rownames exist
+  if (is.null(rownames(metadata_df))) {
+    stop("`metadata_df` must have rownames.")
+  }
+  
+  # Check rownames match colnames of SE
+  if (!all(sort(rownames(metadata_df)) == sort(colnames(se)))) {
+    stop("Rownames of `metadata_df` must match colnames of the SummarizedExperiment object.")
+  }
+  
+  # Return as S4 DataFrame
+  return(S4Vectors::DataFrame(metadata_df))
 }
-
